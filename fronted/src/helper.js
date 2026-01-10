670a072d2ce6b434b862acf1346cf0d943469a96
Rustam-kha <uettaxilarmkhan2454@gmail.com>
2026-01-10 13:09:01 +0500

    Update helper.js

--- fronted/src/helper.js
+++ fronted/src/helper.js
@@ -1,18 +1,22 @@
-// import axios from "axios"
+
+
+// import axios from "axios";
 
 // export const getCart = async (user) => {
-//     const res = await axios.get(`http://localhost:5000/api/get-cart/${user._id}`)
-    
-//     const data = await res.data
-//     return data
-// }
-
+//   const res = await axios.get(
+//     `http://localhost:5000/api/get-cart/${user._id}`,
+//     { withCredentials: true }
+//   );
+//   return res.data;
+// };
 import axios from "axios";
+import { API_URL } from "../config";
+
+axios.defaults.withCredentials = true;
 
 export const getCart = async (user) => {
   const res = await axios.get(
-    `http://localhost:5000/api/get-cart/${user._id}`,
-    { withCredentials: true }
+    `${API_URL}/api/get-cart/${user._id}`
   );
   return res.data;
 };
